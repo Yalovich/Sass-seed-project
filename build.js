227447d49e9178029424569cc67cff535b1bd2f8
Idan Yalovich <idan@zest.is>
2020-12-13 19:18:49 +0200

    added file watcher and normalize.css

--- build.js
+++ build.js
@@ -5,18 +5,45 @@ const path = require('path');
 const fs = require('fs');
 
 const argv = process.argv.slice(2);
-const FILENAME = argv.length > 0 ? argv[0] : "seed"; // TODO: use a proper ARGV parser.
+
+let WATCH = false;
+let FILENAME = "seed";
+
+// TODO: use a proper ARGV parser.
+argv.map(arg => {
+    let [ key, value ] = arg.split("=");
+
+    if(!WATCH) WATCH = key === "--watch";
+
+    if(key === "--file") FILENAME = value;
+});
+
 const SUFFIX = `scss`; // TODO: make adjustable from CLI
 
 const SRC_DIR = `${path.resolve(__dirname)}/src`;
 const DIST_DIR = `${path.resolve(__dirname)}/dist`;
 
-console.log("\t>> Entry point: ", `${SRC_DIR}/${FILENAME}.${SUFFIX}`)
+const INPUT_FILE = `${SRC_DIR}/${FILENAME}.${SUFFIX}`;
 
-const result = sass.renderSync({
-    file: `${SRC_DIR}/${FILENAME}.${SUFFIX}`,
-    sourceMap: true
-});
+const build = (INPUT_FILE, DIST_DIR, FILENAME) => {
+    const result = sass.renderSync({
+        file: INPUT_FILE,
+        sourceMap: true
+    });
 
-fs.writeFileSync(`${DIST_DIR}/${FILENAME}.css`, result.css);
-console.log("\t<< Build completed. T:", (result.stats['duration'] / 1000))
+    fs.writeFileSync(`${DIST_DIR}/${FILENAME}.css`, result.css);
+    console.log("\t<< Build completed. T:", (result.stats['duration'] / 1000))
+}
+
+console.log("\t>> Entry point: ", `${INPUT_FILE}`);
+
+if(WATCH)
+{
+    console.log("\t>> Watching mode.");
+
+    fs.watch(SRC_DIR, {}, (change, file) => {
+        if(file === `${FILENAME}.${SUFFIX}`) build(INPUT_FILE, DIST_DIR, FILENAME);
+    });
+}
+
+build(INPUT_FILE, DIST_DIR, FILENAME);
